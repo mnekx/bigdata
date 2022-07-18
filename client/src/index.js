f5c727a2d47dbb4d4f700d0acefbababe6feca1a
mnekx <karimnembac@gmail.com>
2022-07-18 10:19:39 +0300

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Uploader from './features/upload/Uploader';
+import Graph from "./features/graph/Graph";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index path="upload" element={<Uploader />}></Route>
+          <Route path="graph" element={<Graph/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
